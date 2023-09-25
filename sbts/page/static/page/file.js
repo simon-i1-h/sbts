9b715ed2d25177df8ed0a08903ed7b4bd99bfdb5
Itoh Shimon <shimon@postfixnotation.org>
2023-09-26 00:36:06 +0900

    アップロードボタンを改良

--- sbts/page/static/page/file.js
+++ sbts/page/static/page/file.js
@@ -1,18 +1,29 @@
+let uploadfile = document.querySelector('#uploadfile');
+let uploadbutton = document.querySelector('#uploadbutton');
 let uploadform = document.querySelector('#uploadform');
 let const_map = JSON.parse(document.querySelector('#file-data').text);
 let url_map = const_map['url_map'];
+
+uploadbutton.addEventListener('click', ev => {
+    uploadfile.click();
+});
+
+uploadfile.addEventListener('change', ev => {
+    if (uploadfile.files.length === 1) {
+        uploadform.requestSubmit();
+    }
+});
 
 uploadform.addEventListener('submit', async ev => {
     ev.preventDefault();
 
     let csrf_token = document.querySelector('[name=csrfmiddlewaretoken]').value;
-    let file_elem = document.querySelector('#file');
 
-    if (file_elem.files.length !== 1) {
+    if (uploadfile.files.length !== 1) {
         return;
     }
 
-    let file = file_elem.files[0];
+    let file = uploadfile.files[0];
 
     try {
         let resp = await fetch(url_map['file:upload'], {
