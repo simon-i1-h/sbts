312730ae4f29fcf815853d010dcb79460be79b1b
Itoh Shimon <shimon@postfixnotation.org>
2023-10-01 22:39:02 +0900

    テストスクリプトに引数を渡せるように

--- run_tests_from_host.sh
+++ run_tests_from_host.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-set -e
+set -eu
 
 CODEDIR=/home/app/opt/sbts
-exec docker compose run --rm app gosu app "$CODEDIR/envw" python "$CODEDIR/manage.py" test "$CODEDIR/sbts"
+
+if [ $# -eq 0 ]; then
+  set -- "$CODEDIR/sbts"
+fi
+
+exec docker compose run --rm app gosu app "$CODEDIR/envw" python "$CODEDIR/manage.py" test "$@"
