c9409e2ce0e27e163dab6180a70866aaaf51c5b1
Itoh Shimon <shimon@postfixnotation.org>
2023-10-01 22:54:18 +0900

    テストスクリプト内のパス修正

--- run_tests_from_host.sh
+++ run_tests_from_host.sh
@@ -2,10 +2,9 @@
 
 set -eu
 
-CODEDIR=/home/app/opt/sbts
-
 if [ $# -eq 0 ]; then
-  set -- "$CODEDIR/sbts"
+  set -- sbts
 fi
 
+CODEDIR=/home/app/opt/sbts
 exec docker compose run --rm app gosu app "$CODEDIR/envw" python "$CODEDIR/manage.py" test "$@"
