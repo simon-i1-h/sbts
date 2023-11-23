4a7784610218c9f92175aa428bdf5c3c4610bb0f
Itoh Shimon <shimon@postfixnotation.org>
2023-11-23 15:48:23 +0900

    リファクタリング(URL)

--- sbts/page/urls.py
+++ sbts/page/urls.py
@@ -8,12 +8,12 @@ app_name = 'page'
 urlpatterns = [
     path('', TopPageView.as_view(), name='top'),
     path('file/', FilePageView.as_view(), name='file'),
-    path('file/create/', CreateFileView.as_view(), name='create_file'),
     path('ticket/', TicketPageView.as_view(), name='ticket'),
-    path('ticket/create/', CreateTicketView.as_view(), name='create_ticket'),
     path('ticket/<uuid:key>/', TicketDetailPageView.as_view(), name='ticket_detail'),
-    path('ticket/<uuid:key>/create/', CreateCommentView.as_view(), name='create_comment'),
     path('login/', LoginPageView.as_view(), name='login'),
     path('logout/', LogoutPageView.as_view(), name='logout'),
+    path('api/page/files/', CreateFileView.as_view(), name='create_file'),
+    path('api/page/tickets/', CreateTicketView.as_view(), name='create_ticket'),
+    path('api/page/comments/<uuid:key>/', CreateCommentView.as_view(), name='create_comment'),
     path('api/file/', include('sbts.file.urls')),
 ]
